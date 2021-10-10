8487e4e55a2d860df1c37aae6444f41d5f66c6e4
Brian Archibald <brian.archibald@imtapps.com>
2021-10-10 12:58:24 -0500

    added user authentication

--- blog_project/posts/views.py
+++ blog_project/posts/views.py
@@ -4,11 +4,12 @@ from .models import Post
 from .serializers import PostSerializer
 
 
-class PostList(generics.ListCreatAPIView):
+class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
